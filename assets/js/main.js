58e622e366990a87bc14a0f3bb8162f3b1c94290
Brunno <rivasbrunno10@gmail.com>
2025-09-07 22:44:47 -0300

    nuevos js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,139 +1,9 @@
-const calzados = [
-    {
-        id: "calz-01",
-        titulo: "Air Forece 1 Triple Wiite",
-        precio: 112000,
-        imagen: "../assets/img/AF1 WHITE.webp",
-        genero: {
-            id: "hombre",
-            nombre: "Hombre"
-        },
-        marca:{
-            id: "nike",
-            nombre: "Nike"
-        },
-        color:{
-            id: "axul",
-            nombre: "Azul"
-        },
-        famosos:{
-            id: "badbunny",
-            nombre: "Badbunny"
-        }
-    },
-    {
-        id: "calz-02",
-        titulo: "Nike P-6000 Metallic Silver",
-        precio: 136000,
-        imagen: "../assets/img/NikeP-6000 'Metallic Silver.webp",
-        genero: {
-            id: "hombre",
-            nombre: "Hombre"
-        },
-        marca:{
-            id: "nike",
-            nombre: "Nike"
-        },
-        color:{
-            id: "axul",
-            nombre: "Azul"
-        },
-        famosos:{
-            id: "badbunny",
-            nombre: "Badbunny"
-        }
-    },
-    {
-        id: "calz-03",
-        titulo: "Puma Sneakers Gold",
-        precio: 89000,
-        imagen: "../assets/img/PUMA Sneakers Gold.webp",
-        genero: {
-            id: "mujer",
-            nombre: "Mujer"
-        },
-        marca:{
-            id: "puma",
-            nombre: "Puma"
-        },
-        color:{
-            id: "axul",
-            nombre: "Azul"
-        },
-        famosos:{
-            id: "badbunny",
-            nombre: "Badbunny"
-        }
-    },
-    {
-        id: "calz-04",
-        titulo: "Puma Speedcat OG",
-        precio: 80000,
-        imagen: "../assets/img/PUMASpeedcat OG 'Haute Coffee.webp",
-        genero: {
-            id: "mujer",
-            nombre: "mujer"
-        },
-        marca:{
-            id: "puma",
-            nombre: "Puma"
-        },
-        color:{
-            id: "axul",
-            nombre: "Azul"
-        },
-        famosos:{
-            id: "badbunny",
-            nombre: "Badbunny"
-        }
-    },
-    {
-        id: "calz-05",
-        titulo: "Adidas Superstar Iridescent",
-        precio: 94000,
-        imagen: "../assets/img/adidas Superstar 'Iridescent.webp",
-        genero: {
-            id: "mujer",
-            nombre: "Mujer"
-        },
-        marca:{
-            id: "adidas",
-            nombre: "Adidas"
-        },
-        color:{
-            id: "negro",
-            nombre: "Negro"
-        },
-        famosos:{
-            id: "madona",
-            nombre: "Madona"
-        }
-    },
-    {
-        id: "calz-06",
-        titulo: "Adidas Samba OG Black Cat",
-        precio: 94000,
-        imagen: "../assets/img/adidasSamba OG 'Black Cat.webp",
-        genero: {
-            id: "hombre",
-            nombre: "Hombre"
-        },
-        marca:{
-            id: "adidas",
-            nombre: "Adidas"
-        },
-        color:{
-            id: "axul",
-            nombre: "Azul"
-        },
-        famosos:{
-            id: "badbunny",
-            nombre: "Badbunny"
-        }
-    }
-]
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("calzados").innerHTML = cargarCalzados();
+});
 
 const contenedorCalzado = document.querySelector(".productos")
+const numeroCarrito = document.querySelector(".navbar-cart a")
 
 function cargarCalzados(){
     calzados.forEach(calzado => {
@@ -185,9 +55,11 @@ function agregarProductosCarrito(e){
 }
 
 function actualizarNumeroCarrito(){
-    // funciona como el contador
     let numero = calzadosCarrito.reduce((acc, calzado) => acc + calzado.cantidad, 0)
-    numeroCarrito.innerHTML = numero
+    
+    if (numeroCarrito) {
+        numeroCarrito.innerHTML = `<i class="fa-solid fa-cart-shopping"></i> <span style="color: white;">${numero}</span>`;
+    }
 }
 
 
